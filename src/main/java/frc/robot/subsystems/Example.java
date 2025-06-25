98592280f86f26cce03320e88b05fe5496fe062d
vincent <2496131935@qq.com>
2025-06-25 05:33:16 -0400

    123

--- src/main/java/frc/robot/subsystems/Example.java
+++ src/main/java/frc/robot/subsystems/Example.java
@@ -1,5 +1,5 @@
 package frc.robot.subsystems;
-//this is vincent
+//this is vincent V1.0
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
